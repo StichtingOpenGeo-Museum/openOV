0d9303db1b0cc13c32dbb4eb04bc05740cd0a83c
Stefan de Konink <stefan@konink.de>
2011-07-03 21:06:45 +0200

    Implement the operational date switching after midnight.

--- scrapers/arriva/realtime.py
+++ scrapers/arriva/realtime.py
@@ -1,20 +1,31 @@
 import sys
+import datetime
 from BeautifulSoup import BeautifulSoup
 
 f = open(sys.argv[1])
 
 soup = BeautifulSoup(f.read())
 
+operdate = soup.find('input', {"id" : "realtime-date" })['value']
+operdate = datetime.datetime.strptime(operdate, "%d-%m-%Y").date()
+
 realtime = soup.find('table', {"id" : "realtime-table" })
 
 if realtime is not None:
+	prev = 0
 	for row in realtime.findAll('tr')[1:]:
 		items = row.findAll('td')
 		[linenumber, linename] = items[0].text[5:].split(' naar ')
 		target = items[1].text
 		expected = items[2].text
 
+		now = int(target.split(':')[0])
+		if prev > now:
+			operdate = operdate.replace(day = operdate.day + 1)
+
+		prev = now
+
 		if expected == '-':
 			expected = ''
 
-		print '%s\t%s\t%s\t%s' % (linenumber, linename, target, expected)
+		print '%s\t%s\t%s\t%s\t%s' % (operdate, linenumber, linename, target, expected)
