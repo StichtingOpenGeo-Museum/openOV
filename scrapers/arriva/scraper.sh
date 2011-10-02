00a4506c63b0f945812e8137fe77aa45d6c601cf
Stefan de Konink <stefan@konink.de>
2011-10-02 11:35:15 +0200

    Arriva update, url changed.

--- scrapers/arriva/scraper.sh
+++ scrapers/arriva/scraper.sh
@@ -4,6 +4,11 @@ mkdir -p tmp
 cd tmp
 
 # Thanks to the wonderful implementation of CouchDB that doesn't allow replication we just implement a Conexxion alike give me all you got.
-wget -O arriva-stops.js "http://www.arriva.nl/couchdb-proxy.php?/topology/_design/stops/_view/clusterZ14?startkey=[1,1]&endkey=[20000,20000]"
+for x in `seq 166 171`; do
+	for y in `seq 105 109`; do
+		wget -O "arriva-stops-$x-$y.js" "http://www.arriva.nl/couchdb-proxy.php?/topology/_design/stops/_view/clusterZ14?startkey=[$((${x} * 100 + 1)),$((${y} * 100 + 1))]&endkey=[$((($x + 1) * 100)),$((($y + 1) * 100))]"
+		sleep 1
+	done
+done
 
-wget -O tmp/id-stop.html "http://www.arriva.nl/nc/reisinformatie/actuele-vertrektijden/detailpagina/?tx_bwrealtime_pi1%5Bstop%5D=74670020"
+#wget -O tmp/id-stop.html http://www.arriva.nl/nc/reisinformatie/vertrektijden-zoeken/actuele-vertrektijden/detailpagina/?tx_bwrealtime_pi1[stop]=74670020
