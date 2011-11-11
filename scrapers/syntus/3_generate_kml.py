12d554f71e605b872bd65ac7dd7a9dd2429965b9
Stefan de Konink <stefan@konink.de>
2011-11-11 02:56:28 +0100

    Few fixes

--- scrapers/syntus/3_generate_kml.py
+++ scrapers/syntus/3_generate_kml.py
@@ -10,15 +10,16 @@ from polyline_decode import decode_line
 
 output = '<?xml version="1.0" encoding="UTF-8"?><kml xmlns="http://earth.google.com/kml/2.2">\n'
 
-for (root, dirs, files) in os.walk('xml_polyline'):
+for (root, dirs, files) in os.walk('polyline_xml'):
     for f in files:
-        x = ET.parse('%s/%s'%('xml_polyline', f)).getroot()
+        x = ET.parse('%s/%s'%('polyline_xml', f)).getroot()
         polyline = x.find('.//PolyLine').text
         polyline = decode_line(polyline)
         coordinates = ' '.join([str(t[0])+','+str(t[1]) for t in polyline])
         route = x.find('.//route').text
         lijnid = x.find('.//lijnNummer').text
-        output += '<Document id="%s"><Placemark id="%s"><name>%s</name><LineString><coordinates>%s</coordinates></LineString></Placemark>\n'%(f, lijnid, route, coordinates)
+        richting = x.find('.//richting').text
+        output += '<Document id="%s"><Placemark id="%s_%s"><name>%s</name><LineString><coordinates>%s</coordinates></LineString></Placemark>\n'%(f, lijnid, richting, route, coordinates)
 
         haltes = x.findall('.//Halte')
         for halte in haltes:
